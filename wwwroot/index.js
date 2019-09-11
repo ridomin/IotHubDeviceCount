fff09c7f13e011aab2e27cc3b3f98c4749860430
ridomin <ridomin@live.com>
2019-09-11 02:40:53 -0700

    cleanup index.js

--- wwwroot/index.js
+++ wwwroot/index.js
@@ -1,25 +1,21 @@
 (()=>{
     const numDevices=document.getElementById('numDevices')
-    const time=document.getElementById("currentTime")
-    const form = document.getElementById('form')
     const inputCS=document.getElementById('connectionstring')
 
-    let isConfigured = false
     let connectionString = ''
 
     fetch('/api/connection-string')
         .then((resp)=>{return resp.json()})
         .then((json)=>{
             connectionString=JSON.stringify(json)
-            isConfigured=connectionString.length>0
+            let isConfigured=connectionString.length>0
             console.log(connectionString)
             inputCS.value=connectionString
             setInterval(()=>{
-                //numDevices.innerText=''
                 currentTime.innerText=new Date().toLocaleTimeString()
                 fetch('/api/deviceCount')
                     .then((resp)=>{return resp.json()})
-                    .then((json)=>{numDevices.innerText=JSON.stringify(json)})
+                    .then((json)=>{numDevices.innerText=json})
                 }, 1000);
             })
 })()
