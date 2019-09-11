58ec045a6655212680e82b284f8d660ffa921641
ridomin <ridomin@live.com>
2019-09-11 00:30:54 -0700

    rename

--- wwwroot/index.js
+++ wwwroot/index.js
@@ -1,11 +1,11 @@
 (()=>{
-    const main=document.getElementById('numDevices')
+    const numDevices=document.getElementById('numDevices')
     const time=document.getElementById("currentTime")
     setInterval(()=>{
-        //main.innerText=''
+        //numDevices.innerText=''
         currentTime.innerText=new Date().toLocaleTimeString()
         fetch('/api/deviceCount')
             .then((resp)=>{return resp.json()})
-            .then((json)=>{main.innerText=JSON.stringify(json)})
+            .then((json)=>{numDevices.innerText=JSON.stringify(json)})
         }, 1000);
 })()
