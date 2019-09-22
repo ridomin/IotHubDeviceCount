b1478399950a14510c4228c5fdf89896b3ebea07
ridomin <ridomin@live.com>
2019-09-22 01:22:24 -0700

    add autorefresh

--- wwwroot/index.js
+++ wwwroot/index.js
@@ -6,16 +6,45 @@ function getHubNameFrom(connectionstring){
     return hubName
 }
 
-function bind(hubName,devices) {
+let loadedTime = new Date()
+let _hubName = ""
+const REFRESH_TIMER = 5000
+
+function createVueApp(devices) {
+    var intervalId
     var app = new Vue({
         el: '#deviceList',
         data: {
-            hub: hubName,
+            hub: _hubName,
             devices: devices,
-            now: new Date().toLocaleTimeString()
+            now: new Date().toLocaleTimeString(),
+            elapsed: ''
+        },
+        methods: {
+            toggleAutoRefresh : function(event) {
+                if (event.srcElement.checked) {
+                 intervalTime = new Date()   
+                 intervalId = setInterval(()=>{
+                     currentTime = new Date()
+                     this.elapsed = Math.round(Math.abs((REFRESH_TIMER-(currentTime-intervalTime))/1000)) //moment(currentTime).from(intervalTime)
+                    if (currentTime-intervalTime>REFRESH_TIMER){
+                        console.log("timer")
+                        intervalTime = currentTime
+                        fetch('/api/deviceList')
+                        .then( resp => resp.json())
+                        .then( devicesDto => devices = devicesDto)
+                    } else {
+                        //console.log("wait")
+                    }
+                    }, 1000)
+                } else {
+                    clearInterval(intervalId)                        
+                }
+            }
         }
     })
 }
+
 
 fetch('/api/connection-string')
 .then(resp=> resp.json())
@@ -23,16 +52,12 @@ fetch('/api/connection-string')
     connectionstring.value=json
     if (json.length<10) {
         connectionstring.value="set connection string"
-        formConnectionString.collapse('show')
+       
     } else {
-        hubName = getHubNameFrom(json)
-        //  setInterval(()=>{
-
-            fetch('/api/deviceList')
-                .then( resp => resp.json())
-                .then( devices => bind(hubName,devices))
-            
-          //      }, 5000)
-            }
-        })
+        _hubName = getHubNameFrom(json)
+        fetch('/api/deviceList')
+            .then( resp => resp.json())
+            .then( devices => createVueApp(devices))           
+        }
+    })
 })()
