db39f41831b552be06d957c982f4a784183eb794
ridomin <ridomin@live.com>
2019-09-27 19:14:52 -0700

    apply standard rules

--- app.js
+++ app.js
@@ -1,70 +1,71 @@
-const express = require('express'),
-      app = express(),
-      router = express.Router()
+const express = require('express')
+const path = require('path')
+const app = express()
+const router = express.Router()
 
 const bodyParser = require('body-parser')
 const hub = require('./app.iothub.js')
 const port = 3000
 
-let connectionString=''
+let connectionString = ''
 
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }))
 app.use('/api', router)
-app.use(express.static('wwwroot'))  
+app.use(express.static('wwwroot'))
 
-router.get('/', (req, res, next) => res.sendFile('index.html', {root: __dirname + "wwwroot/index.html"}))
+router.get('/', (req, res, next) => res.sendFile('index.html', { root: path.join(__dirname, 'wwwroot/index.html') }))
 
-router.get('/connection-string', (req,res)=> {
-    if (connectionString && connectionString.length>0) {
-        const hubRegex = /(?<=HostName=).*(?=;SharedAccessKeyName)/i.exec(connectionString)
-        const hubName = hubRegex.length > 0 ? hubRegex[0] : ''
-        res.json(hubName)
-    } else {
-        res.json("not configured")
-    }
+router.get('/connection-string', (req, res) => {
+  if (connectionString && connectionString.length > 0) {
+    const hubRegex = /(?<=HostName=).*(?=;SharedAccessKeyName)/i.exec(connectionString)
+    const hubName = hubRegex.length > 0 ? hubRegex[0] : ''
+    res.json(hubName)
+  } else {
+    res.json('not configured')
+  }
 })
-router.post('/connection-string',(req,res)=> {
-    connectionString = req.body.connectionstring
-    if (connectionString && connectionString.length>0) {
-        const hubRegex = /(?<=HostName=).*(?=;SharedAccessKeyName)/i.exec(connectionString)
-        const hubName = hubRegex.length > 0 ? hubRegex[0] : ''
-        res.json(hubName)
-    } else {
-        res.json("not configured")
-    }
+router.post('/connection-string', (req, res) => {
+  connectionString = req.body.connectionstring
+  if (connectionString && connectionString.length > 0) {
+    const hubRegex = /(?<=HostName=).*(?=;SharedAccessKeyName)/i.exec(connectionString)
+    const hubName = hubRegex.length > 0 ? hubRegex[0] : ''
+    res.json(hubName)
+  } else {
+    res.json('not configured')
+  }
 })
 
-router.get('/deviceList', (req,res) => {
-    if (connectionString.length>0) {
-        hub.getDeviceList(connectionString, list => res.json(list))
-    } else {
-        res.json({})
-    }
+router.get('/deviceList', (req, res) => {
+  if (connectionString.length > 0) {
+    hub.getDeviceList(connectionString, list => res.json(list))
+  } else {
+    res.json({})
+  }
 })
 
 router.get('/getInterfaces', async (req, res) => {
-    const result = await hub.getInterfaces(connectionString, req.query.deviceId)
-    console.log(`getInterfaces on ${req.query.deviceId}`)
-    res.json(result)
+  const result = await hub.getInterfaces(connectionString, req.query.deviceId)
+  console.log(`getInterfaces on ${req.query.deviceId}`)
+  res.json(result)
 })
 
 router.get('/getInterfaceDetails', async (req, res) => {
-    const result = await hub.getInterfaceDetails(connectionString, req.query.urn)
-    console.log(`getInterfaceDetails on ${req.query.urn}`)
-    res.json(result)
+  const result = await hub.getInterfaceDetails(connectionString, req.query.urn)
+  console.log(`getInterfaceDetails on ${req.query.urn}`)
+  res.json(result)
 })
-  
-router.post('/runCommand', async (req, res) => {
-    const result = await hub.runCommand(
-      connectionString,
-      req.query.deviceId,
-      req.query.interfaceName,
-      req.query.command,
-      req.query.param)
-  
-    console.log(`runCommand on ${req.query.deviceId}/${req.query.interfaceName}/${req.query.command}/${req.query.param}\r\nResponse: ${JSON.stringify(result)}`)
-  
-    res.json(result);
-  })
 
-app.listen(port, () => console.log(`IoT Express app listening on port ${port}`));
+router.post('/runCommand', async (req, res) => {
+  const result = await hub.runCommand(
+    connectionString,
+    req.query.deviceId,
+    req.query.interfaceName,
+    req.query.command,
+    req.query.param)
+
+  console.log(`runCommand on ${req.query.deviceId}/${req.query.interfaceName}/${req.query.command}/${req.query.param}\r\nResponse: ${JSON.stringify(result)}`)
+
+  res.json(result)
+})
+
+app.listen(port, () => console.log(`IoT Express app listening on port ${port}`))
--- wwwroot/index.js
+++ wwwroot/index.js
@@ -1,97 +1,100 @@
-(()=>{
-
-function createVueApp() {
+/* global connectionstring, $ */
+(() => {
+  function createVueApp () {
     let intervalId
-    let asc = 1            
-    
-    let app = new Vue({
-        el: '#deviceList',
-        data: {
-            hub: "not set",
-            devices: [],
-            deviceStatus : {},
-            elapsed: '5',
-            refresh: 5000,
-            refreshEnabled: false,
-            loading : false
+    let asc = 1
+
+    const app = new Vue({
+      el: '#deviceList',
+      data: {
+        hub: 'not set',
+        devices: [],
+        deviceStatus: {},
+        elapsed: '5',
+        refresh: 5000,
+        refreshEnabled: false,
+        loading: false
+      },
+      methods: {
+        sortBy: function (by) {
+          if (this.devices.length > 0) {
+            this.devices.sort((a, b) => a[by] > b[by] ? asc : -asc)
+            asc = -asc
+          }
         },
-        methods: {
-            sortBy: function(by) {
-               if (this.devices.length>0) {
-                    this.devices.sort((a,b)=> a[by]>b[by] ? asc : -asc)
-                    asc = -asc
-               }
-            },
-            updateRefresh: function(event) {
-                let interval = parseInt(prompt("Seconds to refresh", this.refresh /1000), 10) * 1000
-                if (isNaN(interval)) interval=5000
-                this.refresh = interval
-            },
-            refreshCount: function(){
-                this.deviceStatus.Disconnected =  this.devices.filter(d=>d.state==='Disconnected').length
-                this.deviceStatus.Connected = this.devices.filter(d=>d.state==='Connected').length
-                this.deviceStatus.Total = this.devices.length
-                this.loading=false
-            },
-            refreshDevices : async function() {
-                this.loading = true;
-                await fetch('/api/deviceList')
-                        .then( resp => resp.json())
-                        .then( devicesDto => {
-                            this.devices = devicesDto
-                            this.refreshCount()
-                        })
-            },
-            postConnectionString: async function(event) {
-                console.log(connectionstring.value)
-                if (connectionstring.value.length>0){
-                    await fetch("/api/connection-string",
-                            {
-                                method: 'POST',
-                                headers : { 
-                                    "Content-Type": "application/x-www-form-urlencoded"
-                                },
-                                body : `connectionstring=${encodeURIComponent(connectionstring.value)}`
-                            })
-                            .then(resp=>resp.json())
-                            .then(text=> this.hub=text)
-                    await this.refreshDevices()
-                    $('#formConnectionString').collapse('hide')
-                }
-            },
-            toggleAutoRefresh : function(event) {
-                this.refreshEnabled=event.srcElement.checked
-                if (this.refreshEnabled) {
-                 intervalTime = new Date()   
-                 intervalId = setInterval(async()=>{
-                    currentTime = new Date()
-                    this.elapsed = Math.round((this.refresh-Math.abs(currentTime-intervalTime))/1000) + 1 //moment(currentTime).from(intervalTime)
-                    if (currentTime-intervalTime>this.refresh){
-                        console.log("timer")
-                        intervalTime = currentTime
-                        await this.refreshDevices()
-                    } else {
-                        //console.log("wait")
-                    }
-                    }, 1000)
-                } else {
-                    clearInterval(intervalId)                        
-                }
-            }
+        updateRefresh: function (event) {
+          let interval = parseInt(prompt('Seconds to refresh', this.refresh / 1000), 10) * 1000
+          if (isNaN(interval)) interval = 5000
+          this.refresh = interval
+        },
+        refreshCount: function () {
+          this.deviceStatus.Disconnected = this.devices.filter(d => d.state === 'Disconnected').length
+          this.deviceStatus.Connected = this.devices.filter(d => d.state === 'Connected').length
+          this.deviceStatus.Total = this.devices.length
+          this.loading = false
+        },
+        refreshDevices: async function () {
+          this.loading = true
+          await fetch('/api/deviceList')
+            .then(resp => resp.json())
+            .then(devicesDto => {
+              this.devices = devicesDto
+              this.refreshCount()
+            })
+        },
+        postConnectionString: async function (event) {
+          console.log(connectionstring.value)
+          if (connectionstring.value.length > 0) {
+            await fetch('/api/connection-string',
+              {
+                method: 'POST',
+                headers: {
+                  'Content-Type': 'application/x-www-form-urlencoded'
+                },
+                body: `connectionstring=${encodeURIComponent(connectionstring.value)}`
+              })
+              .then(resp => resp.json())
+              .then(text => {
+                this.hub = text
+              })
+            await this.refreshDevices()
+            $('#formConnectionString').collapse('hide')
+          }
+        },
+        toggleAutoRefresh: function (event) {
+          this.refreshEnabled = event.srcElement.checked
+          if (this.refreshEnabled) {
+            let intervalTime = new Date()
+            intervalId = setInterval(async () => {
+              const currentTime = new Date()
+              this.elapsed = Math.round((this.refresh - Math.abs(currentTime - intervalTime)) / 1000) + 1 // moment(currentTime).from(intervalTime)
+              if (currentTime - intervalTime > this.refresh) {
+                console.log('timer')
+                intervalTime = currentTime
+                await this.refreshDevices()
+              } else {
+                // console.log("wait")
+              }
+            }, 1000)
+          } else {
+            clearInterval(intervalId)
+          }
         }
+      }
     })
     return app
-}
+  }
 
-var app = createVueApp()
+  var app = createVueApp()
 
-fetch('/api/connection-string')
-    .then(resp=> resp.json())
-    .then(json=>{
-        if (json.length<20) {
-            app.hub="<not configured>"
-        } else {
-            app.hub=json
-            app.refreshDevices()
-        }})
+  fetch('/api/connection-string')
+    .then(resp => resp.json())
+    .then(json => {
+      if (json.length < 20) {
+        app.hub = '<not configured>'
+      } else {
+        app.hub = json
+        app.refreshDevices()
+      }
+    })
 })()
